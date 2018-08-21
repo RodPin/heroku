d0b9d0ab19d0b881f8508d1fea28646051408606
RodPin <prodrigopcgomes@gmail.com>
2018-08-21 09:32:55 -0300

    'whatever'

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (acessToken, refreshToken, profile, done) => {
       // console.log("acess token :", acessToken);
